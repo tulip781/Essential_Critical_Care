ccea50869ab456bb03c6d50b770d1d0c1373052b
Oliver Inglis <ollie@theinglistwins.com>
2020-04-05 11:37:28 +0100

    devise changed to username

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module EssentialCriticalCare
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :en
+    config.i18n.available_locales = [:en, :lo]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
