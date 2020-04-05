94add2c118d0f7f746fb7dc5e89900059ff3597f
Oliver Inglis <ollie@theinglistwins.com>
2020-04-05 13:08:54 +0100

    contact route fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'about', to: "pages#about"
-  get 'contact', to: "pages#about"
+  get 'contact', to: "pages#contact"
 
   resources :navbar_base_folder, only: [ :show ]
   resources :navbar_base_folder, only: [ :show ]
