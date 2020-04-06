09586e91bb003e7c34e49da602564a7e21383d45
Oliver Inglis <ollie@theinglistwins.com>
2020-04-06 19:57:21 +0100

    sorting added to category model

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -6,4 +6,18 @@ class SubCategory < ApplicationRecord
   translates :title, :description
   accepts_nested_attributes_for :category
   globalize_accessors :locales => [:en, :lo], :attributes => [:title, :description]
+
+  def create_resources
+    resources = []
+    self.pdfs.each do |p|
+      resources << p
+    end
+    self.videos.each do |v|
+      resources << v
+    end
+    self.infographics.each do |i|
+      resources << i
+    end
+    resources.sort_by(&:created_at).reverse
+  end
 end
