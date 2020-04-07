f3f106f38881770cd6213e5152eccd891a306095
Oliver Inglis <ollie@theinglistwins.com>
2020-04-07 10:17:50 +0100

    Sort code refactor

--- app/models/secret_sub_category.rb
+++ app/models/secret_sub_category.rb
@@ -7,28 +7,15 @@ class SecretSubCategory < ApplicationRecord
   globalize_accessors :locales => [:en, :lo], :attributes => [:title, :description]
 
   def create_resources
-    resources = []
-    self.pdfs.each do |p|
-      resources << p
-    end
-    self.videos.each do |v|
-      resources << v
-    end
-    self.infographics.each do |i|
-      resources << i
-    end
-    sorted = resources.sort_by(&:created_at).reverse
+    sorted = (self.pdfs + self.videos + self.infographics).sort_by(&:created_at).reverse
     sorted.sort do |a, b|
-      if b.pinned? && a.pinned?
-        0
-      elsif b.pinned? == false && a.pinned?
+      if b.pinned? == false && a.pinned?
         -1
       elsif b.pinned? && a.pinned? == false
         1
-      elsif b.pinned? == false && a.pinned? == false
+      else
         0
       end
     end
   end
-
 end
