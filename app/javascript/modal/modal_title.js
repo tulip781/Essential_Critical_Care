91ebc5cbc8ab1f6d63a1d20975a4a2d6cd12ec33
Oliver Inglis <ollie@theinglistwins.com>
2020-04-10 15:07:00 +0100

    accessiblity updates

--- app/javascript/modal/modal_title.js
+++
@@ -1,9 +0,0 @@
-
-const test = () => {
-  const button = document.querySelector('.pdf-resource-stream-button');
-  button.addEventListener('click', (event) => {
-    console.log("yes yes ")
-  });
-}
-
-export { test };
