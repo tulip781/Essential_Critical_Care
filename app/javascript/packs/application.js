b898833a55cf93940bd285c073eb17b24835c8db
Oliver Inglis <ollie@theinglistwins.com>
2020-04-28 20:22:34 +0100

    Loaders added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,14 @@ import { initModal  } from '../modal/modal';
 
 import { initTopDropNav } from '../side_bar/top_drop_nav';
 
+import { initFader } from '../side_bar/fader';
+
 initModal();
 
 
 initTopDropNav();
+
+
+initFader();
+
+
