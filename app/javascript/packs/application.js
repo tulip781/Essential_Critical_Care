30b09eac816ccc446a8de8d6ad0b4c20b640734a
Oliver Inglis <ollie@theinglistwins.com>
2020-04-28 20:37:43 +0100

    fade homepage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,14 +2,19 @@ import { initModal  } from '../modal/modal';
 
 import { initTopDropNav } from '../side_bar/top_drop_nav';
 
-import { initFader } from '../side_bar/fader';
 
 initModal();
 
 
 initTopDropNav();
 
-
-initFader();
-
-
+window.onload = (event) => {
+  if (document.querySelector('.home-category-image-banner')) {
+  var banner = document.querySelector('.home-category-image-banner');
+    banner.classList.add('fader-ollie');
+    const removeT = () => {
+      banner.classList.remove('fader-ollie');
+    }
+    setTimeout(removeT, 6000);
+  }
+};
